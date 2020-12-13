59da85256e8189a6356bdf5687a32e6cf1e96ca5
Aleh Ryzhankou <ary@bringme.com>
2020-12-13 03:39:17 +0100

    Round 2 RC 3

--- src/main/kotlin/MyStrategy.kt
+++ src/main/kotlin/MyStrategy.kt
@@ -3,7 +3,7 @@ import org.niksi.rai.controller.Controller
 import org.niksi.rai.controller.checkConsistency
 
 class MyStrategy {
-    val version = "1.0.4".also {
+    val version = "1.0.5".also {
         println("Version $it")
     }
     var controller: Controller? = null
