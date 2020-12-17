74535f9fa93a20950a7de69c95dcaf1096746f9f
Aleh Ryzhankou <ary@bringme.com>
2020-12-17 17:44:13 +0100

    Sandbox try 23 from manhattan branch

--- src/main/kotlin/MyStrategy.kt
+++ src/main/kotlin/MyStrategy.kt
@@ -3,7 +3,7 @@ import org.niksi.rai.controller.Controller
 import org.niksi.rai.controller.checkConsistency
 
 class MyStrategy {
-    val version = "1.1.7-manhattan".also {
+    val version = "1.1.8-manhattan".also {
         println("Version $it")
     }
     var controller: Controller? = null
